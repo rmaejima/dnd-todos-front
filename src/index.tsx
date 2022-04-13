64b24b0de1e6f502aeefd284353e38c4fabe2000
rmaejima <ryo.maejii@gmail.com>
2022-04-13 19:04:14 +0900

    Init styled-components theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'ress';
 import './global.css';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import * as theme from 'utils/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
