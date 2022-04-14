af6f09446fb76bd209323929f8aa15e89f07a1be
rmaejima <ryo.maejii@gmail.com>
2022-04-14 18:17:35 +0900

    Add requestPost

--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -1,5 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 
+const API_BASE_URL = 'http://localhost:8080';
+
 export const requestGet = async <T>(url: string): Promise<AxiosResponse<T>> => {
-  return axios.get<T>(url, { baseURL: 'http://localhost:8080' });
+  return axios.get<T>(url, { baseURL: API_BASE_URL });
+};
+
+export const requestPost = async <T, U = undefined>(
+  url: string,
+  payload: U,
+): Promise<AxiosResponse<T>> => {
+  return axios.post<T>(url, payload, { baseURL: API_BASE_URL });
 };
