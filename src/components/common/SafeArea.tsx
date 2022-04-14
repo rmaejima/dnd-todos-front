4ec5310b1ab12d31234af5f5aa86276e277a87ab
rmaejima <ryo.maejii@gmail.com>
2022-04-14 12:14:54 +0900

    Add safearea

---
+++ src/components/common/SafeArea.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SideNav, SIDENAV_WIDTH } from './SideNav';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const SafeArea: React.VFC<Props> = ({ children }) => {
+  return (
+    <Container>
+      <SideNav />
+      <MainContainer>{children}</MainContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${(p) => p.theme.colors.gray[50]};
+
+  margin-left: ${SIDENAV_WIDTH};
+  padding: 1rem 2rem;
+`;
