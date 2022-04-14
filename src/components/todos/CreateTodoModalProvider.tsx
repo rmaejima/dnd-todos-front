6913c7b4efc3f2c37b1a7b7a52c8f5bdaaa586ba
rmaejima <ryo.maejii@gmail.com>
2022-04-14 17:29:33 +0900

    Add tag states

--- src/components/todos/CreateTodoModalProvider.tsx
+++ src/components/todos/CreateTodoModalProvider.tsx
@@ -7,14 +7,39 @@ import { colors } from 'utils/theme';
 import { Button } from 'components/common/Button';
 import { TextField } from 'components/common/TextField';
 import { useState } from 'react';
+import { useAllTags } from 'utils/apis/tag';
+import { Tag } from 'types/tag';
+import { useEffect } from 'react';
+import { TagTip } from './TagTip';
 
 export const CreateTodoModalProvider: React.VFC = () => {
+  const { tags, isLoading, error, refetchAllTags } = useAllTags();
   const [Modal, open, close] = useModal('root', {
     preventScroll: true,
     closeOnOverlayClick: false,
   });
 
   const [titleValue, setTitleValue] = useState<string>('');
+  const [tagSelection, setTagSelection] = useState<Tag[]>([]);
+  const [tagValue, setTagValue] = useState<Tag[]>([]);
+
+  useEffect(() => {
+    if (tags) {
+      setTagSelection(tags);
+    }
+  }, [tags]);
+
+  const onClickTagSelectionTip = (tag: Tag) => {
+    setTagValue([...tagValue, tag]);
+    setTagSelection(
+      tagSelection.filter((selection) => selection.id !== tag.id),
+    );
+  };
+
+  const onClickTagValueTip = (tag: Tag) => {
+    setTagSelection([tag, ...tagSelection]);
+    setTagValue(tagValue.filter((t) => t.id !== tag.id));
+  };
 
   return (
     <>
@@ -32,6 +57,28 @@ export const CreateTodoModalProvider: React.VFC = () => {
             placeholder="Reactの勉強"
             onChange={setTitleValue}
           />
+          <Label>選択タグ</Label>
+          <TipListContainer>
+            {tagValue &&
+              tagValue.map((tag) => (
+                <TagTip
+                  key={tag.id}
+                  tag={tag}
+                  onClick={() => onClickTagValueTip(tag)}
+                />
+              ))}
+          </TipListContainer>
+          <Label>タグリスト</Label>
+          <TipListContainer>
+            {tagSelection &&
+              tagSelection.map((tag) => (
+                <TagTip
+                  key={tag.id}
+                  tag={tag}
+                  onClick={() => onClickTagSelectionTip(tag)}
+                />
+              ))}
+          </TipListContainer>
           <Button color={colors.error[500]} onClick={close}>
             キャンセル
           </Button>
@@ -73,4 +120,12 @@ const Label = styled.label`
 
 const StyledTextField = styled(TextField)`
   margin-bottom: 1rem;
+`;
+
+const TipListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 0.5rem 1rem;
+
+  margin: 1rem 0;
 `;
