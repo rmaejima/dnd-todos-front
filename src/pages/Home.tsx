58f20066276625a20fa42b160b09d743adb7a030
rmaejima <ryo.maejii@gmail.com>
2022-04-14 12:15:05 +0900

    Add title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ImCalendar } from 'react-icons/im';
+import { SafeArea } from 'components/common/SafeArea';
 
 export const Home: React.VFC = () => {
-  return <>home</>;
+  return (
+    <SafeArea>
+      <IconTitle>
+        <ImCalendar />
+        <h1>TODO</h1>
+      </IconTitle>
+    </SafeArea>
+  );
 };
+
+const IconTitle = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: ${(p) => p.theme.colors.text.base};
+  font-size: 1.5rem;
+  font-weight: bold;
+
+  > h1 {
+    margin-left: 1rem;
+    color: ${(p) => p.theme.colors.text.base};
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
