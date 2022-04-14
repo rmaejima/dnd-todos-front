8ac6b1e94d44b760429c14d778e314f188f91731
rmaejima <ryo.maejii@gmail.com>
2022-04-14 12:14:43 +0900

    Add sidenav

---
+++ src/components/common/SideNav.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ImCalendar } from 'react-icons/im';
+import { ImPriceTag } from 'react-icons/im';
+import { ImCheckboxChecked } from 'react-icons/im';
+import { ImBin2 } from 'react-icons/im';
+import { Link } from 'react-router-dom';
+
+export const SIDENAV_WIDTH = '30rem';
+
+export const SideNav: React.VFC = () => {
+  return (
+    <Container>
+      <Title>Wantedly Todo</Title>
+      <LinkArea>
+        <li>
+          <StyledLink to="/">
+            <LinkBar>
+              <ImCalendar />
+              <p>TODO</p>
+            </LinkBar>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/tags">
+            <LinkBar>
+              <ImPriceTag />
+              <p>タグ</p>
+            </LinkBar>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/history">
+            <LinkBar>
+              <ImCheckboxChecked />
+              <p>履歴</p>
+            </LinkBar>
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/archived">
+            <LinkBar>
+              <ImBin2 />
+              <p>ゴミ箱</p>
+            </LinkBar>
+          </StyledLink>
+        </li>
+      </LinkArea>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  width: ${SIDENAV_WIDTH};
+  height: 100%;
+  box-shadow: ${(p) => p.theme.shadows.lg};
+
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 2rem;
+  color: ${(p) => p.theme.colors.text.base};
+  font-size: 1.5rem;
+`;
+
+const LinkArea = styled.ul`
+  > li {
+    list-style: none;
+  }
+
+  > li:not(:first-child) {
+    margin-top: 1rem;
+  }
+`;
+
+const LinkBar = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  padding: 1rem 2rem;
+
+  color: ${(p) => p.theme.colors.text.light};
+  font-size: 1.25rem;
+  font-weight: bold;
+
+  transition: 0.3s ${(p) => p.theme.easings.easeOut};
+  cursor: pointer;
+
+  > p {
+    margin-left: 1rem;
+  }
+
+  &:hover {
+    opacity: 0.7;
+    box-shadow: ${(p) => p.theme.shadows.md};
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
