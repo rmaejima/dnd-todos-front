6c619549f05a0ef2c76c25e0ed37a8f41d48545e
rmaejima <ryo.maejii@gmail.com>
2022-04-15 11:27:47 +0900

    Add route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from 'pages/Home';
 import { NotFound } from 'pages/NotFound';
+import { Finished } from 'pages/Finished';
 
 export const Router: React.VFC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/finished" element={<Finished />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
