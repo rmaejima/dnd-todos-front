e3fc6f5f7d7b8ebb1b7f738dba137537df5dba22
rmaejima <ryo.maejii@gmail.com>
2022-04-15 13:06:45 +0900

    Layout archived todo list

---
+++ src/components/todos/archived/ArchivedTodoList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useAllTodos } from 'utils/apis/todo';
+import { TodoCard } from '../TodoCard';
+
+export const ArchivedTodoList: React.VFC = () => {
+  const { todos, isLoading, error } = useAllTodos({ archived: true });
+  return (
+    <>
+      {error && 'エラーが発生しました'}
+      {isLoading && '読み込み中です'}
+
+      <CardListContainer>
+        {todos && todos.length !== 0 ? (
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <TodoCard todo={todo} disabled />
+            </li>
+          ))
+        ) : (
+          <EmptyMessage>ゴミ箱は空です</EmptyMessage>
+        )}
+      </CardListContainer>
+    </>
+  );
+};
+
+const CardListContainer = styled.ul`
+  list-style: none;
+  > li:not(:first-child) {
+    margin-top: 0.5rem;
+  }
+`;
+
+const EmptyMessage = styled.p`
+  color: ${(p) => p.theme.colors.text.base};
+  font-size: 1rem;
+  font-weight: bold;
+`;
