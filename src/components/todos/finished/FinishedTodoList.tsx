feb64d42998c8267702318fa02e9ba4cd8efd530
rmaejima <ryo.maejii@gmail.com>
2022-04-15 12:51:14 +0900

    Layout finished todo list

--- src/components/todos/finished/FinishedTodoList.tsx
+++ src/components/todos/finished/FinishedTodoList.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useAllTodos } from 'utils/apis/todo';
+import { TodoCard } from '../TodoCard';
 
 export const FinishedTodoList: React.VFC = () => {
-  const { todos } = useAllTodos({ finished: true });
-  return <></>;
+  const { todos, isLoading, error } = useAllTodos({ finished: true });
+  return (
+    <>
+      {error && 'エラーが発生しました'}
+      {isLoading && '読み込み中です'}
+
+      <CardListContainer>
+        {todos &&
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <TodoCard todo={todo} disabled />
+            </li>
+          ))}
+      </CardListContainer>
+    </>
+  );
 };
+
+const CardListContainer = styled.ul`
+  list-style: none;
+  > li:not(:first-child) {
+    margin-top: 0.5rem;
+  }
+`;
