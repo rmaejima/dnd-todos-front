ffa35849cfb1ffe271457ab22f492bdf5b911d85
rmaejima <ryo.maejii@gmail.com>
2022-04-15 16:45:43 +0900

    Fix tip layout

--- src/components/tags/TagTip.tsx
+++ src/components/tags/TagTip.tsx
@@ -11,20 +11,19 @@ interface Props {
 export const TagTip: React.VFC<Props> = ({ tag, onClick }) => {
   return (
     <Container onClick={onClick} $bgColor={validateColorCode(tag.color)}>
-      <p>{tag.title}</p>
+      {tag.title}
     </Container>
   );
 };
 
 const Container = styled.div<{ $bgColor: string }>`
+  display: inline-block;
   background-color: ${(p) => p.$bgColor};
   border-radius: 0.75rem;
   padding: 0.25rem 1rem;
-  color: #fff;
-  text-align: center;
 
-  > p {
-    font-size: 0.75rem;
-    font-weight: bold;
-  }
+  text-align: center;
+  color: #fff;
+  font-size: 0.75rem;
+  font-weight: bold;
 `;
