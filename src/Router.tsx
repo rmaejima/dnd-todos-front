1267addad14f850a455969865b42b69c531429b7
rmaejima <ryo.maejii@gmail.com>
2022-04-15 13:07:06 +0900

    Add archived route

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from 'pages/Home';
 import { NotFound } from 'pages/NotFound';
 import { Finished } from 'pages/Finished';
+import { Archived } from 'pages/Archived';
 
 export const Router: React.VFC = () => {
   return (
@@ -10,6 +11,7 @@ export const Router: React.VFC = () => {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/finished" element={<Finished />} />
+        <Route path="/archived" element={<Archived />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
