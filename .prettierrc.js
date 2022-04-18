ece36335854ae3e69ef60a45c9f43b9b62ad0baa
rmaejima <ryo.maejii@gmail.com>
2022-04-18 15:18:32 +0900

    Add import rule

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,7 @@ module.exports = {
   trailingComma: 'all',
   semi: true,
   singleQuote: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
